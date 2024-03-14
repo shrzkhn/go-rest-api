b5e80ffb2d8ba895b87a31b92e5918bdea13decc
Shiraz Khan <47772492+shrzkhn@users.noreply.github.com>
2024-03-14 21:44:07 +0100

    update model

--- models/city.go
+++ models/city.go
@@ -1,9 +1,7 @@
 package models
 
-import "github.com/google/uuid"
-
 type City struct {
-	Id      uuid.UUID `json:"id" gorm:"type:uuid;default:uuid_generate_v4()"`
-	Name    string    `json:"name"`
-	Country string    `json:"country"`
+	Id      string `json:"id" gorm:"primary_key"`
+	Name    string `json:"name"`
+	Country string `json:"country"`
 }
